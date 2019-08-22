82f3e019ca923e70917c7c5492c6eb3e434cfbb0
reneaas <reneaas@student.matnat.uio.no>
2019-08-22 12:11:51 +0200

    update brah

---
+++ cmake-build-debug/CMakeFiles/fys3150.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fys3150.dir/main.cpp.o"
+  "fys3150.pdb"
+  "fys3150"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fys3150.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
