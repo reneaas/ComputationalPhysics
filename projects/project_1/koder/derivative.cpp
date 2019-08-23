4df680cf58802178157b0a6f6fdde78db5f4a067
reneaas <reneaas@student.matnat.uio.no>
2019-08-23 13:58:42 +0200

    update

--- projects/project_1/koder/derivative.cpp
+++ projects/project_1/koder/derivative.cpp
@@ -9,9 +9,9 @@ double f(double);   //Declaration
 int main(int argc, char* argv[]){
   double derivative;
   double x = atof(argv[1]);
-  double h = 0.5;
+  double h = atof(argv[2]);
   derivative = (f(x+h) - f(x))/h;
-  cout << derivative < endl;
+  cout << derivative << endl;
   return 0;
 }
 
