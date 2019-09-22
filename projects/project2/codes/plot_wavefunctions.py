d19b8e2ca4de2cb59ef7313faf7249d180eeb8e8
reneaas <reneaas@student.matnat.uio.no>
2019-09-22 03:42:56 +0200

    update

--- projects/project2/codes/plot_wavefunctions.py
+++ projects/project2/codes/plot_wavefunctions.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import os
 import sys
 from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
 
@@ -13,26 +12,29 @@ rho_max = float(input("rho_max = "))
 angular_frequencies = [0.01, 0.05, 0.1, 0.5, 1.0]
 
 if repulsion == "yes":
-    figurename = "ground_state_wavefunctions_with_repulsion_rho_max_" + str(rho_max) + ".eps"
+    figurename = "ground_state_wavefunctions_with_repulsion_rho_max_" + str(rho_max) + ".png"
     for angular_frequency in angular_frequencies:
         outfilename = "results/QM_TwoElectrons/Repulsion/wavefunctions/ground_state_n_"\
                     + str(n) + "_omega_" + str(angular_frequency) + "_repulsion_yes.txt"
         wavefunction = []
         rho = []
+        wavefunction.append(0.0);
+        rho.append(0.0);
         with open(outfilename, "r") as outfile:
             lines = outfile.readlines()
             for line in lines:
                 numbers = line.split()
                 wavefunction.append(float(numbers[0]))
                 rho.append(float(numbers[-1]))
+        wavefunction.append(0.0)
+        rho.append(rho_max)
         plt.plot(rho, wavefunction, label = r"$\omega = $" + str(angular_frequency))
 
 
     plt.ylabel(r"$|\psi (\rho )|^2$", fontsize = 18)
     plt.xlabel(r"$\rho$", fontsize = 18)
     plt.legend()
-    #plt.title(r"Ground states (including repulsion) for different angular angular_frequencies")
-    plt.savefig(figurename, dpi = 400)
+    plt.savefig(figurename, dpi = 1000)
     plt.close()
 
     path = "results/QM_TwoElectrons/Repulsion/plots"
@@ -41,26 +43,29 @@ if repulsion == "yes":
     os.system("mv" + " " + figurename + " " + path)
 
 if repulsion == "no":
-    figurename = "ground_state_wavefunctions_no_repulsion_rho_max_" + str(rho_max) + ".eps"
+    figurename = "ground_state_wavefunctions_no_repulsion_rho_max_" + str(rho_max) + ".png"
     for angular_frequency in angular_frequencies:
         outfilename = "results/QM_TwoElectrons/NoRepulsion/wavefunctions/ground_state_n_"\
                     + str(n) + "_omega_" + str(angular_frequency) + "_repulsion_no.txt"
         wavefunction = []
         rho = []
+        wavefunction.append(0.0)
+        rho.append(0.0)
         with open(outfilename, "r") as outfile:
             lines = outfile.readlines()
             for line in lines:
                 numbers = line.split()
                 wavefunction.append(float(numbers[0]))
                 rho.append(float(numbers[-1]))
+        wavefunction.append(0.0)
+        rho.append(rho_max)
         plt.plot(rho, wavefunction, label = r"$\omega = $" + str(angular_frequency))
 
 
     plt.ylabel(r"$|\psi (\rho )|^2$", fontsize = 18)
     plt.xlabel(r"$\rho$", fontsize = 18)
     plt.legend()
-    #plt.title("Ground states (no repulsion) for different angular angular_frequencies")
-    plt.savefig(figurename, dpi = 400)
+    plt.savefig(figurename, dpi = 1000)
     plt.close()
 
     path = "results/QM_TwoElectrons/NoRepulsion/plots"
