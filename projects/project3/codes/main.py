a0acf597a1ec19368c6023bc9077a9f97c4af669
reneaas <reneaas@student.matnat.uio.no>
2019-10-09 16:37:10 +0200

    update

--- projects/project3/codes/main.py
+++ projects/project3/codes/main.py
@@ -1,8 +1,15 @@
 import os
+import sys
+
+compilation_instruction = str(sys.argv[1])
+if compilation_instruction == "1":
+    print("compiling")
+    os.system("c++ -O3 -c main.cpp lib.cpp")
+    os.system("c++ -O3 -o main.exe main.cpp lib.o")
+    print("executing")
+    os.system("./main.exe")
 
 
-print("compiling")
-os.system("c++ -O3 -c main.cpp lib.cpp")
-os.system("c++ -O3 -o main.exe main.cpp lib.o")
-print("executing")
-os.system("./main.exe")
+if compilation_instruction == "0":
+    print("executing")
+    os.system("./main.exe")
