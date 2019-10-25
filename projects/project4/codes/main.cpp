df240bfab07561855cd7edd2cf7224de2a26d0db
Maria Linea Horgen <marialinea1@hotmail.com>
2019-10-25 18:05:18 +0200

    Må fikse magnetiseringen

--- projects/project4/codes/main.cpp
+++ projects/project4/codes/main.cpp
@@ -14,6 +14,7 @@ using namespace  std;
 
 void initialize(int, int **, double&, double&);
 
+void Monte_Carlo_Metropolis(int, int, int **, int, double&, double&, double&, double&, double*);
 
 ofstream ofile;               //Global variable for writing results to file.
 
@@ -23,10 +24,10 @@ inline int periodic(int coordinate, int dimensions, int step) {
 
 int main(int nargs, char* args[]){
 
-  string outfilename, number_of_temperatures;
-  double E, M,boltzmann_distribution[9];
+  string outfilename;
+  double E, M,boltzmann_distribution[17];
   double T_initial, T_final, step_size;   //Variables to define temperature vector
-  int n, MC_cycles;
+  int n, MC_cycles, J, number_of_temperatures;
 
   //Read from command line
   number_of_temperatures = atoi(args[1]);
@@ -35,11 +36,6 @@ int main(int nargs, char* args[]){
   n = atoi(args[3]);                    //Dimension of spin matrix
   MC_cycles = atoi(args[4]);            //Number of Monte Carlo cycles
 
-  if (number_of_temperatures != 1){
-    T_initial = atof(args[5]);         //Inital temperature
-    T_final = atof(args[6]);           //Final temperature
-    step_size = atof(args[7]);
-  }
 
   //initialize matrix
   int **spin_matrix;
@@ -48,63 +44,50 @@ int main(int nargs, char* args[]){
     spin_matrix[i] = new int[n];
   }
 
-  //Computing the boltzmann distribution for 5 values of dE
-  double beta = 1/(T);                //k_B = 1
-  for (int i = 0; i < 9; i+=4){
-    boltzmann_distribution[i] = exp(-beta*i);
-  }
-
   E = 0;
   M = 0;
 
   //filling spin matrix with arbitrary spin values
   initialize(n, spin_matrix, E, M);
 
-  random_device rd;
-  mt19937_64 gen(rd());
-  uniform_int_distribution<int> RandomIntegerGenerator(0,n-1);        //Sets up the uniform distribution for x in [0,n-1]
-  uniform_real_distribution<double> RandomNumberGenerator(0,1);       //Sets up the uniform distribution for x in [0,1]
 
-    int x_flip, y_flip, J;
-    double dE, dM, dE_squared, dM_squared;                //Change in energy and magnetization
 
-    dE_squared = 0;
-    dM_squared = 0;
+  double E_squared, M_squared;                //Change in energy and magnetization
 
-    J = 1;
+  E_squared = E*E;
+  M_squared = M*M;
 
-  //Running over Monte Carlo samples
-  for (int k = 0; k < MC; k++){
 
-    x_flip = RandomIntegerGenerator(gen);
-    y_flip = RandomIntegerGenerator(gen);      //Randomized indices to matrix element that will be flipped
+  J = 1;                                     //Coupling constant
 
-    spin_matrix[x_flip][y_flip] *= (-1);
+  if (number_of_temperatures == 1){
+    double T = atof(args[5]);
 
-    dM = M + 2*spin_matrix[x_flip][y_flip]*spin_matrix[x_flip][y_flip];
-
-    for (int i = 0; i < n; i++){
-      for (int j = 0; j < n; j++){
-        dE = (double) 2*J*spin_matrix[x_flip][y_flip] * (spin_matrix[periodic(x_flip,dimensions,1)][y_flip] + spin_matrix[periodic(x_flip, dimensions,-1)][y_flip] + spin_matrix[x_flip][periodic(y_flip, dimensions,1)] + spin_matrix[x_flip][periodic(y_flip, dimensions,-1)]);
-      }
+    //Computing the boltzmann distribution for 5 values of dE
+    double beta = 1/(T);                //k_B = 1
+    for (int i = -8; i < 9; i+=4){
+      boltzmann_distribution[i + 8] = exp(-beta*i);
     }
 
-    //Metropolis algorithm
-    if (dE >= 0){
-      r = RandomNumberGenerator(gen);
-      P = boltzmann_distribution[dE];          //Probability from boltzmann distribution
-      if (r > P){
-        spin_matrix[x_flip][y_flip] *= (-1);   //Rejecting the flip
+    Monte_Carlo_Metropolis(MC_cycles, n, spin_matrix, J, E, M, E_squared,  M_squared, boltzmann_distribution);
 
-      }
-    }
 
-    E += dE;                //Computing change in energy
-    M += dM;                //Computing change in magnetization
-    dE_squared += dE*dE;
-    dM_squared += dM*dM;
   }
 
+
+  if (number_of_temperatures > 1){
+    T_initial = atof(args[5]);         //Inital temperature
+    T_final = atof(args[6]);           //Final temperature
+    step_size = atof(args[7]);
+
+
+    double* temperatures;
+    temperatures = new double[number_of_temperatures+1];
+    for (int i = 0; i <= number_of_temperatures; i++){
+      temperatures[i] = T_initial + (double) step_size*i;
+    }
+
+  }
 
   return 0;
 }
@@ -126,17 +109,64 @@ void initialize(int dimensions, int **spin_matrix, double& E, double& M){
   }
 
 
-void Monte_Carlo_Metropolis(int J, ){
+void Monte_Carlo_Metropolis(int MC, int n, int **spin_matrix, int J, double& E, double& M, double& E_squared, double& M_squared, double* boltzmann_distribution){
 
   random_device rd;
   mt19937_64 gen(rd());
   uniform_int_distribution<int> RandomIntegerGenerator(0,n-1);        //Sets up the uniform distribution for x in [0,n-1]
   uniform_real_distribution<double> RandomNumberGenerator(0,1);       //Sets up the uniform distribution for x in [0,1]
 
-  int x_flip, y_flip;
+  int x_flip, y_flip, dE, dM, nn;
+  double E_sum, M_sum;
+
+  nn = n*n;
+
+  E_sum = 0;
+  M_sum = 0;
+  //Running over Monte Carlo samples
+  for (int k = 0; k < MC; k++){
 
 
+    for (int j = 0; j < nn; j++){
+      x_flip = RandomIntegerGenerator(gen);
+      y_flip = RandomIntegerGenerator(gen);      //Randomized indices to matrix element that will be flipped
 
 
+      dE =  2*J*spin_matrix[x_flip][y_flip] * (spin_matrix[periodic(x_flip,n,1)][y_flip] + spin_matrix[periodic(x_flip, n,-1)][y_flip] + spin_matrix[x_flip][periodic(y_flip, n,1)] + spin_matrix[x_flip][periodic(y_flip, n,-1)]);
+
+      //Metropolis algorithm
+      if(dE < 0){
+        spin_matrix[x_flip][y_flip] *= (-1);   //Accepting the flip
+        dM = 2*spin_matrix[x_flip][y_flip];
+      }
+      else if(RandomNumberGenerator(gen) < boltzmann_distribution[dE + 8]){
+        spin_matrix[x_flip][y_flip] *= (-1);     //Accepting flip
+        dM = 2*spin_matrix[x_flip][y_flip];
+      }
+      else{
+        dE = 0;
+        dM = 0;
+      }
+
+      E += dE;
+      M += dM;
+
+    }
+
+    E_sum += (double) E;
+    M_sum += (double) M;
+
+    E_squared += (double) E_sum*E_sum;
+    M_squared += (double) dM*dM;
+  }
+
+  E_sum /= (double) (MC);
+  M_sum /= (double) MC;
+  E_squared /= MC;
+  M_squared /= MC;
+
+
+  cout<<"Magnetization:" << M_sum <<endl;
+  cout << "E = " << E_sum << endl;
 
 }
---
+++ projects/project4/codes/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+
+
+
+print("compiling")
+os.system("c++ -O3 -c main.cpp")
+os.system("c++ -O3 -o main.exe main.cpp")
+
+number_of_temperatures = int(input("Number of temperatures: "))
+
+if number_of_temperatures == 1:
+    T = float(input("T: "))
+    MC_cycles = int(input("Number of Monte Carlo cycles: "))
+    n_spins = int(input("Number of spins in each direction = "))
+    outfilename = "random.txt"
+
+
+    print("executing")
+    command_line_args = str(number_of_temperatures) + " " + outfilename + " " + str(n_spins) + " " + str(MC_cycles) + " " + str(T)
+    os.system("./main.exe" + " " + command_line_args)
+    #antall temp - outfilename - grid - MC_cycles - temp
+
+else:
+
+    T_initial = float(input("T_initial? "))
+    T_final = float(input("T_final? "))
+    step_size = (T_final - T_initial)/number_of_temperatures
