2212e125fe6bb6859c673b4b86ebe727e1efd2af
reneaas <reneaas@student.matnat.uio.no>
2019-10-30 00:13:12 +0100

    fixed some stuff with the plotting

--- projects/project4/codes/main.py
+++ projects/project4/codes/main.py
@@ -43,7 +43,7 @@ if part == "c":
         initialize = "random"
 
     outfilename = "MC_" + str(MC_samples) + "_n_" + dimension + "_T_" + str(temperature) + "_" + initialize + "_.txt"
-    outfilename2 = "boltzmann_distribution_T_" + str(temperature) + ".txt"
+    outfilename2 = "boltzmann_distribution_T_" + str(temperature) + "_" + initialize + ".txt"
 
     command_line_args = number_of_temperatures + " " + outfilename + " " + dimension \
                     + " " + str(MC_samples) + " " + initialize + " " + str(temperature) + " " + outfilename2
