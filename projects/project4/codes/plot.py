e6f8065ec66daf9b03ca017000c2344a10cee6d6
Maria Linea Horgen <marialinea1@hotmail.com>
2019-10-30 11:19:53 +0100

    update

--- projects/project4/codes/plot.py
+++ projects/project4/codes/plot.py
@@ -37,28 +37,6 @@ if part == "c":
             acceptance_random.append(float(values[3]))
 
 
-<<<<<<< HEAD
-    plt.plot(time[:1000000], E_ordered[:1000000], label = "E ordered")
-    plt.plot(time[:1000000], E_random[:1000000], label = "E random")
-    plt.xlabel("t [cycles/spins]")
-    plt.ylabel("E/spins")
-    plt.legend()
-    plt.figure()
-
-    plt.plot(time[:1000000], M_ordered[:1000000], label = "M ordered")
-    plt.plot(time[:1000000], M_random[:1000000], label = "M random")
-    plt.xlabel("t [cycles/spins]")
-    plt.ylabel("M/spins")
-    plt.legend()
-    plt.figure()
-
-    plt.plot(time[:1000000], acceptance_random[:1000000], label = "Accepted states (random)")
-    plt.plot(time[:1000000], acceptance_ordered[:1000000], label = "Accepted states (ordered)")
-    plt.xlabel("t [cycles/spins]")
-    plt.ylabel("Accepted spins")
-    plt.legend()
-    ##plt.figure()
-=======
     plt.plot(time[:], E_ordered[:], label = "ground state initiation")
     plt.plot(time[:], E_random[:], label = "random initiation")
     plt.xlabel(r"$t$ [cycles/spins]", size = 14)
@@ -84,15 +62,10 @@ if part == "c":
     plt.xticks(size = 14)
     plt.yticks(size = 14)
     plt.legend(fontsize = 12)
->>>>>>> c173681c84ccc419ea7091e8d08c1ce0f3b4eda3
     plt.show()
 
 if part == "d":
     energies = []
-<<<<<<< HEAD
-    temperature = float(input("Temperature: [1.0 or 2.4] \n"))
-    infilename =  "boltzmann_distribution_T_" + str(temperature) + ".txt"
-=======
     temperature = float(input("Temperature = "))
     initial_spin_state = str(input("ordered or random: [o/r]"))
     if initial_spin_state == "o":
@@ -101,7 +74,6 @@ if part == "d":
         initial_spin_state = "random"
 
     infilename =  "boltzmann_distribution_T_" + str(temperature) + "_" + initial_spin_state + ".txt"
->>>>>>> c173681c84ccc419ea7091e8d08c1ce0f3b4eda3
     path = "results/partC/"
     with open(path + infilename, "r") as infile:
         lines = infile.readlines()
@@ -109,9 +81,5 @@ if part == "d":
             values = line.split()
             energies.append(float(values[0]))
 
-<<<<<<< HEAD
-    plt.hist(energies[1000000:], 401, density = True)
-=======
     plt.hist(energies, 800 + 1, density = True)
->>>>>>> c173681c84ccc419ea7091e8d08c1ce0f3b4eda3
     plt.show()
