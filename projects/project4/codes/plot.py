542d53ac3eb52de7cf3a40caac0822fab418d667
reneaas <reneaas@student.matnat.uio.no>
2019-11-10 20:01:01 +0100

    update

--- projects/project4/codes/plot.py
+++ projects/project4/codes/plot.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import sys
 import pandas as pd
 import os
+from scipy.interpolate import UnivariateSpline
+from scipy.optimize import curve_fit
 plt.rc("text", usetex = True)
 
 part = str(input("Which part of the project to run: [b,c,d,e] \n"))
@@ -295,8 +297,8 @@ if part == "b":
 if part == "c":
     T = float(input("Give temperature: "))
     path = "results/partC/"
-    infilename_ordered = "MC_" + str(int(4e8)) + "_n_20_T_" + str(T) + "_ordered_.txt"
-    infilename_random = "MC_" + str(int(4e8)) + "_n_20_T_" + str(T) + "_random_.txt"
+    infilename_ordered = "MC_" + str(int(2e8)) + "_n_20_T_" + str(T) + "_ordered_.txt"
+    infilename_random = "MC_" + str(int(2e8)) + "_n_20_T_" + str(T) + "_random_.txt"
     E_ordered = []
     M_ordered = []
     acceptance_ordered = []
@@ -307,7 +309,7 @@ if part == "c":
 
     with open(path + infilename_ordered, "r") as infile:
         infile.readline()
-        lines = infile.readlinn_spins = n*n;es()
+        lines = infile.readlines()
         for line in lines:
             values = line.split()
             time.append(float(values[0]))
@@ -325,8 +327,8 @@ if part == "c":
             acceptance_random.append(float(values[3]))
 
     time = [i/1000 for i in time]
-    plt.plot(time[:500000], E_ordered[:500000], label = "ground state initiation")
-    plt.plot(time[:500000], E_random[:500000], label = "random initiation")
+    plt.plot(time[:], E_ordered[:], label = "ground state initiation")
+    plt.plot(time[:], E_random[:], label = "random initiation")
     plt.xlabel("$t$ [$10^3 \\times$ cycles/$L^2$]", size = 14)
     plt.ylabel(r"$\langle E \rangle / L^2$", size = 14)
     plt.xticks(size = 14)
@@ -553,15 +555,18 @@ if part == "e":
     path = "results/partE/total_time_" + str(total_time) + "burn_in_time_" + str(time) + "/"
     my_ranks = [i for i in range(p)]
     Lattice_sizes = [40, 60, 80, 100]
+    T_C = []                #To store maximum values.
 
     fig1 = plt.figure(); figurename_energy = "energies.pdf"
     fig2 = plt.figure(); figurename_magnetization = "magnetization.pdf"
     fig3 = plt.figure(); figurename_chi = "chi.pdf"
     fig4 = plt.figure(); figurename_heat_capacity = "heat_capacity.pdf"
+    fig5 = plt.figure(); figurename_interpolation = "interpolated_heat_capacity.pdf"
     ax1 = fig1.add_subplot(111);
     ax2 = fig2.add_subplot(111);
     ax3 = fig3.add_subplot(111);
     ax4 = fig4.add_subplot(111);
+    ax5 = fig5.add_subplot(111);
 
     for L in Lattice_sizes:
         T = []
@@ -584,8 +589,7 @@ if part == "e":
         ax1.scatter(T, E, label = str(L) + " x " + str(L), marker = ".")
         ax1.set_xlabel(r"$k_BT$" , size = 14)
         ax1.set_ylabel(r"$\langle E \rangle/L^2J$", size = 14)
-        plt.xticks(size = 14)
-        plt.yticks(size = 14)
+        ax1.tick_params(labelsize = 15)
         ax1.legend(fontsize = 12)
 
 
@@ -593,31 +597,70 @@ if part == "e":
         ax2.scatter(T, M, label = str(L) + " x " + str(L), marker = ".")
         ax2.set_xlabel(r"$k_BT$" , size = 14)
         ax2.set_ylabel(r"$\langle |M| \rangle/L^2$", size = 14)
-        plt.xticks(size = 14)
-        plt.yticks(size = 14)
+        ax2.tick_params(labelsize = 15)
         ax2.legend(fontsize = 12)
 
 
         ax3.scatter(T, chi, label = str(L) + " x " + str(L), marker = ".")
         ax3.set_xlabel(r"$k_BT$" , size = 14)
         ax3.set_ylabel(r"$\chi/L^2$", size = 14)
-        plt.xticks(size = 14)
-        plt.yticks(size = 14)
+        ax3.tick_params(labelsize = 15)
         ax3.legend(fontsize = 12)
 
 
         ax4.scatter(T, Cv, label = str(L) + " x " + str(L), marker = ".")
         ax4.set_xlabel(r"$k_BT$" , size = 14)
         ax4.set_ylabel(r"$ C_V/L^2$", size = 14)
-        plt.xticks(size = 14)
-        plt.yticks(size = 14)
+        ax4.tick_params(labelsize = 15)
         ax4.legend(fontsize = 12)
+
+        #Interpolate dataset and create smoother plots:
+        Cs = UnivariateSpline(T,Cv, s=2)
+        Ts = np.linspace(2.0,2.399,100)
+        Cv_spline = Cs(Ts)
+        ax5.plot(Ts, Cv_spline, label = str(L) + " x " + str(L))
+        ax5.set_xlabel(r"$k_BT$" , size = 14)
+        ax5.set_ylabel(r"$ C_V/L^2$", size = 14)
+        ax5.tick_params(labelsize = 15)
+        ax5.legend(fontsize = 12)
+
+        index = np.where(np.array(Cv_spline) == max(Cv_spline))
+        T_C.append(Ts[np.sum(index)])
+
+    inverse_L = [1/float(i) for i in Lattice_sizes]
+    figurename_critical_temp = "critical_temp.pdf"
+    fig6 = plt.figure()
+    ax6 = fig6.add_subplot(111);
+    ax6.scatter(inverse_L, T_C, marker = "x", color = "r", label = "Datapoints")
+    ax6.set_xlabel(r"$1/L$", size = 16)
+    ax6.set_ylabel(r"$T_C(L)$", size = 16)
+    ax6.tick_params(labelsize = 15)
+    #ax6.legend(fontsize = 12)
+
+    linear_func = lambda x,a,b: a + b*x             #Form of the function to fit.
+    popt, pcov = curve_fit(linear_func, inverse_L, T_C)
+    print("(a,b) = ", popt)
+    X = np.array(inverse_L)
+    L = np.linspace(35,130, 1001)
+    X = [1/i for i in L]
+    X = np.array(X)
+    Y = popt[1]*X + popt[0]
+    ax6.plot(X,Y,"-k")
+    equation = r"$T_C(L) = 1.32077399/L + \underbrace{2.2649641}_{=T_C(\infty)}$"
+    ax6.text(0.005,2.30, equation, {"color":"k", "fontsize": 15})
+    ax6.legend(fontsize = 12)
+
+
+
 
     fig1.savefig(figurename_energy)
     fig2.savefig(figurename_magnetization)
     fig3.savefig(figurename_chi)
     fig4.savefig(figurename_heat_capacity)
+    fig5.savefig(figurename_interpolation)
+    fig6.savefig(figurename_critical_temp)
 
-    figurenames = figurename_energy + " " + figurename_magnetization + " " + figurename_chi + " " + figurename_heat_capacity
+    figurenames = figurename_energy + " " + figurename_magnetization + " " + figurename_chi + " " + figurename_heat_capacity + " " + figurename_interpolation\
+                    + " " + figurename_critical_temp
 
     os.system("mv" + " " + figurenames + " " + path)
